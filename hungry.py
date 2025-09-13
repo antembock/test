7818693f131070a862ffd63841cc9a670cf2e34d
abane <mbockantem@yahoo.com>
2025-09-13 18:39:32 -0500

    water

--- hungry.py
+++ hungry.py
@@ -1,5 +1,8 @@
 hungry=input("are you hungry?:\n")
 if hungry == "yes":
     print("eat Fufu and Eru")
+thirsty = input("are you thirsty?:\n")
+if thirsty == 'yes':
+    print("drink water")
 else:
     print("have some rest")
