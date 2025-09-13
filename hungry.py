c160a3b4755c5ef236a21db81946d7bb9ab80b38
abane <mbockantem@yahoo.com>
2025-09-13 18:50:08 -0500

    fries

--- hungry.py
+++ hungry.py
@@ -1,5 +1,9 @@
 hungry=input("are you hungry?:\n")
 if hungry == "yes":
     print("eat Fufu and Eru")
+    print("eat pizza")
+    print("eat burger")
+    print("eat fries")
+    
 else:
     print("have some rest")
